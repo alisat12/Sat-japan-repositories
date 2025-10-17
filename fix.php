1906eb3b0ef92c968dca5ec9f08842dd5101d1e5
alisat12 <aliraza@satjapan.co>
2025-10-17 11:56:45 +0500

    add subtract method

--- fix.php
+++ fix.php
@@ -14,8 +14,12 @@ class fix {
 
     public function divide($a, $b) {
         if ($b == 0) {
+            echo "this is a test";
             return "Division by zero error!";
         }
         return $a / $b;
+    }
+    public function subtract($a, $b) {
+        return $a - $b;
     }
 }
